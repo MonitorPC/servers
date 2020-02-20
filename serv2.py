2df4f6f61b7f9eb21d33a8273a0348b51026b9f7
MonitorPC <48449929+MonitorPC@users.noreply.github.com>
2020-02-20 20:01:01 +0300

    Add files via upload

--- serv2.py
+++ serv2.py
@@ -10,5 +10,10 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def train(prof):
+    return render_template('in.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
